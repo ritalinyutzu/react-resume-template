5537aedbda726d928dcd04ac378d8adb4d16c905
Rita <39242563+ritalinyutzu@users.noreply.github.com>
2025-10-21 22:27:54 +0800

    force change
    force change
    
    Signed-off-by: Rita <39242563+ritalinyutzu@users.noreply.github.com>

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -14,11 +14,11 @@ const Portfolio: FC = memo(() => {
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
         <h2 className="self-center text-xl font-bold text-white">Check out some of my work</h2>
-        <div className=" w-full columns-2 md:columns-3 lg:columns-4">
+        <div className="flex flex-wrap justify-center gap-6 w-full">
           {portfolioItems.map((item, index) => {
             const {title, image} = item;
             return (
-              <div className="pb-6" key={`${title}-${index}`}>
+              <div className="w-full sm:w-[calc(50%-12px)] md:w-[calc(33.333%-16px)] lg:w-[calc(25%-18px)]" key={`${title}-${index}`}>
                 <div
                   className={classNames(
                     'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
@@ -49,6 +49,7 @@ const ItemOverlay: FC<{item: PortfolioItem}> = memo(({item: {url, title, descrip
       setMobile(true);
     }
   }, []);
+
   useDetectOutsideClick(linkRef, () => setShowOverlay(false));
 
   const handleItemClick = useCallback(
@@ -62,7 +63,7 @@ const ItemOverlay: FC<{item: PortfolioItem}> = memo(({item: {url, title, descrip
   );
 
   return (
-    <a
+    
       className={classNames(
         'absolute inset-0 h-full w-full  bg-gray-900 transition-all duration-300',
         {'opacity-0 hover:opacity-80': !mobile},
