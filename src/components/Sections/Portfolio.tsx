b46c4d46ce62d8e26c71c54fb54403691c8eb060
Rita <39242563+ritalinyutzu@users.noreply.github.com>
2025-10-22 09:37:23 +0800

    force change
    force change
    
    Signed-off-by: Rita <39242563+ritalinyutzu@users.noreply.github.com>

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -14,23 +14,21 @@ const Portfolio: FC = memo(() => {
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
         <h2 className="self-center text-xl font-bold text-white">Check out some of my work</h2>
-        <div className="flex justify-center w-full">
-          <div className="w-full max-w-md">
-            {portfolioItems.map((item, index) => {
-              const {title, image} = item;
-              return (
-                <div className="pb-6" key={`${title}-${index}`}>
-                  <div
-                    className={classNames(
-                      'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
-                    )}>
-                    <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
-                    <ItemOverlay item={item} />
-                  </div>
+        <div className="w-full columns-2 md:columns-3 lg:columns-4">
+          {portfolioItems.map((item, index) => {
+            const {title, image} = item;
+            return (
+              <div className="pb-6" key={`${title}-${index}`}>
+                <div
+                  className={classNames(
+                    'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
+                  )}>
+                  <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
+                  <ItemOverlay item={item} />
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </Section>
