830ffcb0b0f8dbd6f8339afa6bf884d695fc47d1
Rita <39242563+ritalinyutzu@users.noreply.github.com>
2025-10-22 11:06:56 +0800

    force change
    Refactor Resume component to conditionally render certifications section and remove skills section.
    
    Signed-off-by: Rita <39242563+ritalinyutzu@users.noreply.github.com>

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,9 +1,8 @@
 import {FC, memo} from 'react';
 
-import {certifications, education, experience, SectionId, skills} from '../../../data/data';
+import {certifications, education, experience, SectionId} from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
-import {SkillGroup} from './Skills';
 import TimelineItem from './TimelineItem';
 
 const Resume: FC = memo(() => {
@@ -20,24 +19,17 @@ const Resume: FC = memo(() => {
             <TimelineItem item={item} key={`${item.title}-${index}`} />
           ))}
         </ResumeSection>
-        <ResumeSection title="Certifications">
-          {certifications.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection>
-        <ResumeSection title="Skills">
-          <p className="pb-8">My technical skills and expertise</p>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            {skills.map((skillgroup, index) => (
-              <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
+        {certifications.length > 0 && (
+          <ResumeSection title="Certifications">
+            {certifications.map((item, index) => (
+              <TimelineItem item={item} key={`${item.title}-${index}`} />
             ))}
-          </div>
-        </ResumeSection>
+          </ResumeSection>
+        )}
       </div>
     </Section>
   );
 });
 
 Resume.displayName = 'Resume';
-
 export default Resume;
