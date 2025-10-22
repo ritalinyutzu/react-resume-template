5593f3884ad9c761656c921158d232b6b2a4a3d9
Rita <39242563+ritalinyutzu@users.noreply.github.com>
2025-10-22 11:30:00 +0800

    force change
    force change
    
    Signed-off-by: Rita <39242563+ritalinyutzu@users.noreply.github.com>

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -286,18 +286,18 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      name: 'Friedrich Nietzsche',
+      text: 'What is great in man is that he is a bridge and not an end.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      name: 'Friedrich Nietzsche',
+      text: 'You must be ready to burn yourself in your own flame; how could you rise anew if you have not first become ashes?',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      name: 'Friedrich Nietzsche',
+      text: 'One repays a teacher badly if one always remains nothing but a pupil.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
     },
   ],
