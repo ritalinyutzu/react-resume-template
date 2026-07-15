965d1078595507d62799dd2eb7739c9199e1cf4d
Rita <39242563+ritalinyutzu@users.noreply.github.com>
2026-07-16 00:21:27 +0800

    Update Hero.tsx
    Signed-off-by: Rita <39242563+ritalinyutzu@users.noreply.github.com>

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -9,7 +9,12 @@ const Hero: FC = memo(() => {
   return (
     <Section noPadding sectionId={SectionId.Hero}>
       <div className="relative flex h-screen w-full items-center justify-center bg-neutral-900">
-        <h1 className="whitespace-nowrap px-4 text-[length:clamp(1.1rem,4.2vw,3rem)] font-bold text-white">{name}</h1>
+        <div className="flex flex-col items-center gap-y-3 px-4 text-center">
+          <h1 className="whitespace-nowrap text-[length:clamp(1.1rem,4.2vw,3rem)] font-bold text-white">{name}</h1>
+          <p className="text-[length:clamp(0.875rem,2vw,1.25rem)] text-neutral-300">
+            Currently taking on freelance projects
+          </p>
+        </div>
       </div>
     </Section>
   );
